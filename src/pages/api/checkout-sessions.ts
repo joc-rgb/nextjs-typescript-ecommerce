fbb72f42fb783e4aafa03634698358f932e67184
Jocxyen <jocxyen@gmail.com>
2022-12-16 21:01:26 +0800

    Fix stripe webhook error

--- src/pages/api/checkout-sessions.ts
+++ src/pages/api/checkout-sessions.ts
@@ -3,58 +3,59 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe'
 import { CartItemInterface } from 'utils/Interfaces';
 
-const key:string = process.env.STRIPE_SECRET_KEY || ""
+const key: string = process.env.STRIPE_SECRET_KEY || ""
 const stripe = new Stripe(key, {
-    apiVersion: "2022-11-15",
+	apiVersion: "2022-11-15",
 })
- 
+
 export default async (req: NextApiRequest,
-    res: NextApiResponse<any>) => {
-    
-    if (req.method === 'POST') {
-        const { items, email } = req.body;
-        console.log(items);
-        const params:Stripe.Checkout.SessionCreateParams =  {
-				submit_type: "pay",
-				mode: "payment",
-				payment_method_types: ["card"],
-            shipping_options: [{ shipping_rate: "shr_1MCgsaGWVm9zXuLH1nBDnC0Q" }],
-                shipping_address_collection: {allowed_countries: ['US', 'CA','MY','SG']},
-				line_items: items.map((item:CartItemInterface) => {
-                    const img = item.addImg[0];
-					return {
-						price_data: {
-							currency: "usd",
-							product_data: {
-								name: item.name,
-								images: [img],
-							},
-							unit_amount: parseFloat(item.price[0]) * 100,
+	res: NextApiResponse<any>) => {
+
+	if (req.method === 'POST') {
+		const { items, email } = req.body;
+		console.log(items);
+		const params: Stripe.Checkout.SessionCreateParams = {
+			submit_type: "pay",
+			mode: "payment",
+			payment_method_types: ["card"],
+			shipping_options: [{ shipping_rate: "shr_1MCgsaGWVm9zXuLH1nBDnC0Q" }],
+			shipping_address_collection: { allowed_countries: ['US', 'CA', 'MY', 'SG'] },
+			line_items: items.map((item: CartItemInterface) => {
+				const img = item.addImg[0];
+				return {
+					price_data: {
+						currency: "usd",
+						product_data: {
+							name: item.name,
+							images: [img],
 						},
-						adjustable_quantity: {
-							enabled: true,
-							minimum: 1,
-						},
-						quantity: item.quantity,
-					};
-				}),
-				success_url: `${req.headers.origin}/success`,
-            cancel_url: `${req.headers.origin}/`,
-            metadata: {
-                    email
-                }
-			};
-      
-        try {
-            
-			  const session = await stripe.checkout.sessions.create(params);
-          res.status(200).json(session);
-        } catch (err) {
-          console.log(err)
-            return res.status(500).json({
-            message: "Something went wrong!! Please try again after sometime",
-            });
-}
-    }
-   
+						unit_amount: parseFloat(item.price[0]) * 100,
+					},
+					adjustable_quantity: {
+						enabled: true,
+						minimum: 1,
+					},
+					quantity: item.quantity,
+				};
+			}),
+			success_url: `${req.headers.origin}/success`,
+			cancel_url: `${req.headers.origin}/`,
+			metadata: {
+				email,
+
+			}
+		};
+
+		try {
+
+			const session = await stripe.checkout.sessions.create(params);
+			res.status(200).json(session);
+		} catch (err) {
+			console.log(err)
+			return res.status(500).json({
+				message: "Something went wrong!! Please try again after sometime",
+			});
+		}
+	}
+
 }
--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -8,16 +8,16 @@ import Stripe from 'stripe';
 
 //Initialize Firebase App//
 const app = !admin.apps.length
-    ? admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount as ServiceAccount),
-      })
+  ? admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  })
   : admin.app();
-  
+
 const key: string = process.env.STRIPE_SECRET_KEY || ""
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET || ""
-const endpointSecret = process.env.STRIPE_SIGNING_SECRET||"";
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET || "";
 const stripe = new Stripe(key, {
-    apiVersion: "2022-11-15",
+  apiVersion: "2022-11-15",
 })
 
 const cors = Cors({
@@ -26,11 +26,8 @@ const cors = Cors({
 
 const fulfillOrder = async (session: any) => {
   console.log("Fulfilling order", session);
-  
-  const images = JSON.parse(session?.metadata?.images||"").map((image:string) =>
-        JSON.stringify(image)
-    ) 
-    console.log(images)
+
+
   return app.firestore()
     .collection("customers")
     .doc(session?.metadata?.email || "")
@@ -38,10 +35,10 @@ const fulfillOrder = async (session: any) => {
     .doc(session.id)
     .set({
       id: session.id,
-      amount: (session?.amount_total||0 )/ 100,
+      amount: (session?.amount_total || 0) / 100,
       amount_shipping: (session?.total_details?.amount_shipping || 0) / 100,
       timestamp: admin?.firestore.FieldValue.serverTimestamp(),
-      images: images,
+
     })
     .then(() => {
       console.log(
@@ -51,36 +48,36 @@ const fulfillOrder = async (session: any) => {
     .catch((err) => console.log("Error: ", err.message));
 }
 
-export default async (req:NextApiRequest, res:NextApiResponse) => {
-    if (req.method === 'POST') {
-        const requestBuffer = await buffer(req)
-        const payload = requestBuffer.toString()
-        const sign = req.headers["stripe-signature"]
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const requestBuffer = await buffer(req)
+    const payload = requestBuffer.toString()
+    const sign = req.headers["stripe-signature"]
 
-        let event;
-        
-        //Verify event comes from stripe
-        try {
-            event = stripe.webhooks.constructEvent(payload, sign||"", endpointSecret)
-        
-        } catch (err: any) {
-            console.log("ERROR: ",err.message)
-            return res.status(400).send(`Webhook error: ${err.message}`)
-        }
+    let event;
 
-        //Handle checkout session
-        if (event.type === 'checkout.session.completed') {
-            const session = event.data.object
-       
-            //Fulfill order
-            return fulfillOrder(session).then(() => res.status(200)).catch((err)=>res.status(400).send(`Webhook error: ${err.message}`))
-        }
+    //Verify event comes from stripe
+    try {
+      event = stripe.webhooks.constructEvent(payload, sign || "", endpointSecret)
+
+    } catch (err: any) {
+      console.log("ERROR: ", err.message)
+      return res.status(400).send(`Webhook error: ${err.message}`)
     }
+
+    //Handle checkout session
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object
+
+      //Fulfill order
+      return fulfillOrder(session).then(() => res.status(200)).catch((err) => res.status(400).send(`Webhook error: ${err.message}`))
+    }
+  }
 }
 
 export const config = {
-    api: {
-        bodyParser: false,
-        externalResolver: true
-    }
+  api: {
+    bodyParser: false,
+    externalResolver: true
+  }
 }
