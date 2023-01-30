24243acf7041be910609bc09a1d1a0bfb37e7c53
Jocxy <123352164+jocxy@users.noreply.github.com>
2023-01-30 22:05:16 +0800

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     
 <footer className="p-4 bg-white border-t-2 b-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 ">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/jocxyen" className="hover:underline">Jocelin Yen</a>
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/jocxy" className="hover:underline">Jocelin Yen</a>
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 gap-3">
         {links.map(({ href, label }) => (
